43d833630cd8bbcb1fb0dfd655888c97da7b277a
Apridev <apriantofajar11@gmail.com>
2022-07-23 11:13:00 +0900

    app

--- app.js
+++ app.js
@@ -13,8 +13,10 @@ var colorsRouter = require('./app/colors/router');
 var productRouter = require('./app/product/router');
 var cardRouter = require('./app/card/router');
 var paymentRouter = require('./app/payment/router');
+var playersRouter = require('./app/players/router');
 
 var app = express();
+const URL = `/api/v1`
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -41,6 +43,9 @@ app.use('/colors', colorsRouter);
 app.use('/product', productRouter);
 app.use('/card', cardRouter);
 app.use('/payment', paymentRouter);
+
+// API
+app.use(`${URL}/players`, playersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
